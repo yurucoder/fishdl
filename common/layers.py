d2e3fe9524a80cd3cd261eb08d27179d360432da
Hojun Kim <hojunkim02@icloud.com>
2025-12-12 19:34:09 +0900

    오차역전파법 3

--- common/layers.py
+++ common/layers.py
@@ -1,5 +1,9 @@
+import sys, os
 import numpy as np
 from numpy.typing import NDArray
+
+sys.path.append(os.getcwd())
+from common.functions import *
 
 
 class Relu:
@@ -33,5 +37,48 @@ class Sigmoid:
 
     def backward(self, dout):
         dx = dout * (1.0 - self.out) * self.out
+
+        return dx
+
+
+class Affine:
+    def __init__(self, W: NDArray, b: NDArray):
+        self.W = W
+        self.b = b
+        self.x = None
+        self.dW = None
+        self.db = None
+
+    def forward(self, x: NDArray):
+        self.x = x
+        out = np.dot(x, self.W) + self.b
+
+        return out
+
+    def backward(self, dout: NDArray):
+        dx = np.dot(dout, self.W.T)
+        self.dW = np.dot(self.x.T, dout)
+        self.db = np.sum(dout, axis=0)
+
+        return dx
+
+
+# 소프트맥스 함수와 교차 엔트로피오차는 미분의 결과로 출력과 정답 레이블의 오차로 깔끔히 떨어진다.
+# 이는 설계상 의도적인 결과로서, 항등함수와 오차제곱합의 조합도 마찬가지이다.
+class SoftmaxWithLoss:
+    def __init__(self):
+        self.loss = None
+        self.y = None
+        self.t = None
+
+    def forward(self, x: NDArray, t: NDArray):
+        self.t = t
+        self.y = softmax(x)
+        self.loss = cross_entropy_error(self.y, self.t)
+        return self.loss
+
+    def backward(self, dout=1):
+        batch_size = self.t.shape
+        dx = (self.y - self.t) / batch_size
 
         return dx
